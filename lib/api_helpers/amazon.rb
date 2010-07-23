d67a5082879bf31f6b1f054441c8d733b6870b69
Chris McClelland <chrismcc@pumpkin.local>
2010-07-23 00:27:09 -0400

    fix?

--- lib/api_helpers/amazon.rb
+++ lib/api_helpers/amazon.rb
@@ -303,6 +303,7 @@ module Amazon
       offers_box_element = doc.at('div.resultsset')
       offer_type_header_tables = offers_box_element.search('table')
       offer_type_header_tables.each do |offer_type_header_table|
+        
         if offer_type_header_table.inner_text.force_encoding('UTF-8').include?('Featured Merchants')
           featured_offer_rows = offer_type_header_table.search('tbody.result/tr')
           offers += parse_offer_listing_rows(asin, featured_offer_rows, true)
